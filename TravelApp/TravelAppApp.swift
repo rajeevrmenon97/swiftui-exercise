0a385fe70b7eb2d74c68e0d0faf8fda141cf0877
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-10-19 13:15:21 -0600

    Initial commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Rajeev R Menon on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
